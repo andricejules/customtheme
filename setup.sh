e933b1aaf4273f39a287dba2b597da0c09bca85b
andricejules <42875784+andricejules@users.noreply.github.com>
2022-03-07 18:15:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[theme]
-primaryColor="#FF3B3F"
-base="dark"
-backgroundColor="#F5F5F5"
-secondaryBackgroundColor="#85A7FF"
-font="roboto"
-textColor="#000000"
-
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
